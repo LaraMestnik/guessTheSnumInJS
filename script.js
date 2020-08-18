2a482b26b9b7581cc641b0ddde5b6ab80a3ba06d
Lara Mestnik <lara.mestnik@gmail.com>
2020-08-18 11:13:20 +0200

    added JS

--- script.js
+++ script.js
@@ -1,7 +1,29 @@
+let userGuess = parseInt(document.querySelector('#user-guess').value);
+let errorMsg = document.querySelector('#error');
+let result = document.querySelector('#result');
 
-
-
-function createSnum() {
-    let sNum = Math.floor(Math.random() * 100 + 1);
-    console.log(sNum);
+let sNum = function createRandom() {
+    let random = Math.floor(Math.random() * 100 + 1);
+    return random;
 }
+
+
+
+function guessTheSecretNumber() {
+    if (userGuess === sNum) {
+        result.textContent = `Congratulations! You won. The secret number is ${sNum}`;
+    }
+    else if (userGuess < sNum) {
+        result.textContent = 'Your guess is too small. Try something bigger';
+    }
+    else if (userGuess > sNum) {
+        result.textContent = 'Your guess is too big. Try something smaller';
+    }
+    else if (userGuess === '') {
+        errorMsg.textContent = 'Please enter a field';
+    }
+};
+
+
+
+
