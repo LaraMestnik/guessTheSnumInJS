0ac87552f290077154aca4c6e8cfdd1f2399ecd0
Lara Mestnik <lara.mestnik@gmail.com>
2020-10-06 10:46:08 +0200

    fixed error on last condition and styled error msg for empty input

--- script.js
+++ script.js
@@ -11,20 +11,22 @@ button.addEventListener('click', guessTheSecretNumber);
 function guessTheSecretNumber(e) {
     e.preventDefault();
 
-    const userGuess = parseInt(document.querySelector('#user-guess').value);
+    const userGuess = document.querySelector('#user-guess').value;
+    userGuessNum = parseInt(userGuess);
 
-    if (userGuess === sNum) {
+    if (userGuessNum === sNum) {
         result.textContent = `Congratulations! You guessed it! The secret number is ${sNum}`;
         result.className = 'text-success mt-5 text-uppercase font-weight-bold';
     }
-    else if (userGuess < sNum) {
+    else if (userGuessNum < sNum) {
         result.textContent = 'Your guess is too small. Try something bigger';
     }
-    else if (userGuess > sNum) {
+    else if (userGuessNum > sNum) {
         result.textContent = 'Your guess is too big. Try something smaller';
     }
     else if (userGuess === "") {
-        result.textContent = 'Please enter a field';
+        errorMsg.textContent = 'Please enter a field';
+        errorMsg.className = 'text-danger text-uppercase font-weight-bold';
     }
 };
 
